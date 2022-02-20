a4179425768051737c3baf422c3e0d05df8b1e90
Vedant K <gamemaker0042@gmail.com>
2022-02-20 13:31:12 +0530

    test(pages/signin): add incorrect credentials test case

--- tests/integration/signin.test.ts
+++ tests/integration/signin.test.ts
@@ -2,7 +2,7 @@
 // Integration test for the sign in page.
 
 describe('Sign In Page', () => {
-	it('Type in an invalid email', () => {
+	it('should show a validation error when an invalid email is entered', () => {
 		cy.visit('/signin')
 
 		cy.get('#email').type('user!example').should('have.value', 'user!example')
@@ -10,5 +10,15 @@ describe('Sign In Page', () => {
 		cy.get('#signin').click()
 
 		cy.get('#error').should('have.text', 'Please enter a valid email and try again.')
+	})
+
+	it('should show an invalid credentials error when the wrong credentials are entered', () => {
+		cy.visit('/signin')
+
+		cy.get('#email').type('no-one@example.com').should('have.value', 'no-one@example.com')
+		cy.get('#password').type('wrong-secret').should('have.value', 'wrong-secret')
+		cy.get('#signin').click()
+
+		cy.get('#error').should('have.text', 'We could not find a user with that email. Please check the email for typos and try again.')
 	})
 })
